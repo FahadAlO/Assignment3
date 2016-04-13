047f274df435b56e3076d9adaf44162f235fa156
FahidD131 <d13125583@users.noreply.github.com>
2016-04-13 20:44:42 +0100

    Update ViewController.swift

--- ShopGames/ViewController.swift
+++ ShopGames/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController
    let ref = Firebase(url: "https://shopgames.firebaseio.com")
     
        
+  var screen: [String] = ["Xcode.png","Xcode1.png","Xcode2.png","Xcode3.png","Xcode4.png","Xcode5.png","Xcode6.png","Xcode7.png","Xcode8.png","Xcode9.png","Xcode10.png","Xcode11.png","Xcode12.png","Xcode13.png","Xcode14.png","Xcode15.png","email.png","facebook.png","password2.png","Screen.png","Screen10.png","Screen11.png","Screen12.png","Screen13.png","Screen14.png","Screen15.png","Screen2.png"
+    ,"Screen3.png","Screen4.png","Screen5.png","Screen6.png","Screen7.png","Screen8.png","Screen9.png","Screen1.png","tumblr.png","twitter.png","UserName.png"]
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passweordField: UITextField!
+    
+       
+      @IBAction func handleCreateaccount(sender: AnyObject)
+    { 
+       
+       
+       }
+       
     override func viewDidLoad()
     {
         super.viewDidLoad()
